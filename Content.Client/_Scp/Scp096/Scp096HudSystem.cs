425bab6b4fe74e2b72761da83d540900b556a58b
Mona Hmiza <hmizahmizowna@hmiza.hmiza>
2024-08-21 19:55:58 +0300

    Оверлей был ошибкой.

--- Content.Client/_Scp/Scp096/Scp096HudSystem.cs
+++ Content.Client/_Scp/Scp096/Scp096HudSystem.cs
@@ -1,10 +1,13 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using Content.Client._Scp.Scp096.Ui;
 using Content.Client.Overlays;
 using Content.Shared._Scp.Scp096;
 using Content.Shared.Inventory.Events;
 using Content.Shared.StatusIcon.Components;
-using Robust.Client.Graphics;
 using Robust.Client.Player;
+using Robust.Client.UserInterface;
+using Robust.Client.UserInterface.Controls;
+using Robust.Shared.Configuration;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Timing;
 
@@ -14,17 +17,21 @@ public sealed class Scp096HudSystem : EquipmentHudSystem<Scp096Component>
 {
     [Dependency] private readonly IPlayerManager _playerManager = default!;
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
-    [Dependency] private readonly IOverlayManager _overlayManager = default!;
-    [Dependency] private readonly EntityLookupSystem _entityLookup = default!;
-    [Dependency] private readonly IEyeManager _eyeManager = default!;
     [Dependency] private readonly IGameTiming _gameTiming = default!;
+    [Dependency] private readonly IUserInterfaceManager _uiManager = default!;
 
+    private Scp096UiWidget? _widget;
 
     public override void Initialize()
     {
         base.Initialize();
 
-        SubscribeLocalEvent<Scp096TargetComponent, GetStatusIconsEvent>(OnStatusIcon);
+        SubscribeLocalEvent<Scp096TargetComponent, GetStatusIconsEvent>(OnGetStatusIcon);
+
+        _uiManager.OnScreenChanged += (screens) =>
+        {
+            RemoveWidget();
+        };
     }
 
     protected override void UpdateInternal(RefreshEquipmentHudEvent<Scp096Component> args)
@@ -33,26 +40,65 @@ public sealed class Scp096HudSystem : EquipmentHudSystem<Scp096Component>
 
         var player = _playerManager.LocalSession?.AttachedEntity;
 
-        if (_overlayManager.HasOverlay<Scp096Overlay>()
-            || !TryComp<Scp096Component>(player, out var scp096Component))
+        if (!HasComp<Scp096Component>(player))
         {
             return;
         }
 
-        var scpEntity = new Entity<Scp096Component>(player.Value, scp096Component);
-        var scpOverlay = new Scp096Overlay(scpEntity, _entityLookup, _eyeManager, _gameTiming);
+        EnsureWidgetExist();
+    }
 
-        _overlayManager.AddOverlay(scpOverlay);
+    private void EnsureWidgetExist()
+    {
+        if (_uiManager.ActiveScreen == null)
+        {
+            return;
+        }
+
+        var layoutContainer = _uiManager.ActiveScreen.FindControl<LayoutContainer>("ViewportContainer");
+
+        _widget = new Scp096UiWidget();
+        LayoutContainer.SetAnchorAndMarginPreset(_widget, LayoutContainer.LayoutPreset.CenterTop, margin: 50);
+
+        layoutContainer.AddChild(_widget);
+    }
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        if (!IsActive)
+        {
+            return;
+        }
+
+        if (_widget == null)
+        {
+            EnsureWidgetExist();
+            return;
+        }
+
+        if (!TryGetPlayerEntity(out var scpEntity) || !scpEntity.Value.Comp.RageStartTime.HasValue)
+        {
+            _widget.Visible = false;
+            return;
+        }
+
+        _widget.Visible = true;
+
+        var elapsedTime = _gameTiming.CurTime - scpEntity.Value.Comp.RageStartTime;
+        var remainingTime = scpEntity.Value.Comp.RageDuration - elapsedTime.Value.TotalSeconds;
+
+        _widget.SetData(remainingTime, scpEntity.Value.Comp.Targets.Count);
     }
 
     protected override void DeactivateInternal()
     {
         base.DeactivateInternal();
-
-        _overlayManager.RemoveOverlay<Scp096Overlay>();
+        RemoveWidget();
     }
 
-    private void OnStatusIcon(Entity<Scp096TargetComponent> ent, ref GetStatusIconsEvent args)
+    private void OnGetStatusIcon(Entity<Scp096TargetComponent> ent, ref GetStatusIconsEvent args)
     {
         var playerEntity = _playerManager.LocalSession?.AttachedEntity;
 
@@ -73,5 +119,29 @@ public sealed class Scp096HudSystem : EquipmentHudSystem<Scp096Component>
         return IsActive &&
                player.HasValue &&
                HasComp<Scp096Component>(player.Value);
+    }
+
+    private bool TryGetPlayerEntity([NotNullWhen(true)] out Entity<Scp096Component>? scpEntity)
+    {
+        var playerEntity = _playerManager.LocalSession?.AttachedEntity;
+
+        scpEntity = null;
+
+        if (!TryComp<Scp096Component>(playerEntity, out var scp096Component))
+        {
+            return false;
+        }
+
+        scpEntity = new Entity<Scp096Component>(playerEntity.Value, scp096Component);
+
+        return true;
+    }
+
+    private void RemoveWidget()
+    {
+        if (_widget == null) return;
+
+        _widget.Parent?.RemoveChild(_widget);
+        _widget = null;
     }
 }
--- Resources/Locale/ru-RU/_fire/scp/096.ftl
+++ Resources/Locale/ru-RU/_fire/scp/096.ftl
@@ -1,1 +1,4 @@
 ﻿scp096-non-argo-attack-attempt = Я не хочу этого делать
+
+scp096-targets-left-label = Осталось: {$targets} целей.
+scp096-time-left-label = Осталось: {$time} секунд.
