7661e16af0520ce7c939f1e11a4b6b224886dfd1
rhailrake <splatt.pr@gmail.com>
2024-08-24 10:30:45 +0500

    lowp grain

---
+++ Content.Client/_Scp/Grain/GrainSystem.cs
@@ -0,0 +1,15 @@
+﻿using Robust.Client.Graphics;
+
+namespace Content.Client._Scp.Grain;
+
+public sealed class GrainSystem : EntitySystem
+{
+    [Dependency] private readonly IOverlayManager _overlayManager = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        _overlayManager.AddOverlay(new GrainOverlay());
+    }
+}
