49fcdef0838ae8e9ed413b86facd71bc71192042
drdth <drdtheuser@gmail.com>
2024-12-10 19:07:18 +0300

    add awareness helmet logic

---
+++ Content.Shared/_Scp/Other/ClothingAddComponents/ClothingAddComponentsComponent.cs
@@ -0,0 +1,11 @@
+﻿using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._Scp.Other.ClothingAddComponents;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class ClothingAddComponentsComponent : Component
+{
+    [DataField(required:true)]
+    public ComponentRegistry Components = new();
+}
---
+++ Content.Shared/_Scp/Scp035/Scp035MindProtection/Scp035MindProtectionSystem.cs
@@ -0,0 +1,24 @@
+﻿using Robust.Shared.Containers;
+
+namespace Content.Shared._Scp.Scp035.Scp035MindProtection;
+
+public sealed class Scp035MindProtectionSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<Scp035MaskComponent, ContainerGettingInsertedAttemptEvent>(OnEquipAttempt);
+    }
+
+    /// <summary>
+    /// Не дает защищенному игроку взять/надеть SCP-035
+    /// </summary>
+    /// <param name="scp">Защищенный игрок</param>
+    /// <param name="args">Ивент</param>
+    private void OnEquipAttempt(Entity<Scp035MaskComponent> scp, ref ContainerGettingInsertedAttemptEvent args)
+    {
+        if (HasComp<Scp035MindProtectionComponent>(args.Container.Owner))
+            args.Cancel();
+    }
+}
