f2fc436fc68b2ea545f49b7457d272e54f9f0c5f
drdth <drdtheuser@gmail.com>
2025-02-08 00:12:17 +0300

    add: create random item artifacts effect

---
+++ Content.Server/_Scp/Research/Artifacts/Effects/RandomArtifacts/CreateRandomArtifactComponent.cs
@@ -0,0 +1,7 @@
+﻿namespace Content.Server._Scp.Research.Artifacts.Effects.RandomArtifacts;
+
+[RegisterComponent]
+public sealed partial class CreateRandomArtifactsComponent : Component
+{
+
+}
---
+++ Content.Server/_Scp/Research/Artifacts/Effects/RandomArtifacts/RandomArtifactSystem.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using Content.Server.Xenoarchaeology.XenoArtifacts;
+using Content.Server.Xenoarchaeology.XenoArtifacts.Events;
+using Content.Shared.Item;
+using Robust.Shared.Console;
+using Robust.Shared.Random;
+
+namespace Content.Server._Scp.Research.Artifacts.Effects.RandomArtifacts;
+
+public sealed partial class RandomArtifactsSystem : EntitySystem
+{
+    [Dependency] private readonly ArtifactSystem _artifactsSystem = default!;
+    [Dependency] private readonly IConsoleHost _console = default!;
+    [Dependency] private readonly IRobustRandom _random = default!;
+
+    private const float ItemToArtifactRatio = 0.2f; // from 0 to 100. In % percents. Default is 0.2%
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<CreateRandomArtifactsComponent, ArtifactActivatedEvent>(OnActivate);
+
+        _console.RegisterCommand("listartifacts", "Показывает список всех артефактов-предметов, созданных в результате эффекта от изучения SCP", "listartifacts", ListArtifacts);
+    }
+
+    private void OnActivate(Entity<CreateRandomArtifactsComponent> ent, ref ArtifactActivatedEvent args)
+    {
+        var items = EntityQuery<ItemComponent>().ToList();
+        _random.Shuffle(items);
+
+        var selectedItems = GetPercentageOfHashSet(items, ItemToArtifactRatio);
+
+        foreach (var item in selectedItems)
+        {
+            var entity = item.Owner;
+
+            var artifactComponent = EnsureComp<ArtifactComponent>(entity);
+            _artifactsSystem.RandomizeArtifact(entity, artifactComponent);
+        }
+    }
+
+    // TODO: Перенести в ScpHelperSystem и переделать на генерик <T>
+    private HashSet<ItemComponent> GetPercentageOfHashSet(List<ItemComponent> sourceList, float percentage)
+    {
+        var countToAdd = (int) Math.Round((double) sourceList.Count * percentage / 100);
+
+        return sourceList.Where(x => !Transform(x.Owner).Anchored).Take(countToAdd).ToHashSet();
+    }
+}
