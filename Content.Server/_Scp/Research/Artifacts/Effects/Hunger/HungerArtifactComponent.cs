5ca088395199364bfecf16561b53aa48c9917fdd
drdth <drdtheuser@gmail.com>
2025-02-07 11:25:30 +0300

    add: new 2 effects with hunger and thirst

---
+++ Content.Server/_Scp/Research/Artifacts/Effects/Hunger/HungerArtifactComponent.cs
@@ -0,0 +1,8 @@
+﻿namespace Content.Server._Scp.Research.Artifacts.Effects.Hunger;
+
+[RegisterComponent]
+public sealed partial class HungerArtifactComponent : Component
+{
+    [DataField] public float Range = 12f;
+    [DataField] public float Amount = 40f;
+}
---
+++ Content.Server/_Scp/Research/Artifacts/Effects/Thirst/ThirstArtifactSystem.cs
@@ -0,0 +1,31 @@
+﻿using Content.Server.Xenoarchaeology.XenoArtifacts.Events;
+using Content.Shared.Nutrition.Components;
+using Content.Shared.Nutrition.EntitySystems;
+using Robust.Shared.Random;
+
+namespace Content.Server._Scp.Research.Artifacts.Effects.Thirst;
+
+public sealed class ThirstArtifactSystem : EntitySystem
+{
+    [Dependency] private readonly EntityLookupSystem _lookup = default!;
+    [Dependency] private readonly ThirstSystem _thirst = default!;
+    [Dependency] private readonly IRobustRandom _random = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<ThirstArtifactComponent, ArtifactActivatedEvent>(OnActivate);
+    }
+
+    private void OnActivate(Entity<ThirstArtifactComponent> ent, ref ArtifactActivatedEvent args)
+    {
+        var humans = _lookup.GetEntitiesInRange<ThirstComponent>(Transform(ent).Coordinates, ent.Comp.Range);
+
+        foreach (var uid in humans)
+        {
+            var modifier = _random.NextFloat(-1f, 1f);
+            _thirst.ModifyThirst(uid, uid, modifier * ent.Comp.Amount);
+        }
+    }
+}
