768be1fbccc4372b4c47ceb37e939871cc143bce
drdth <drdtheuser@gmail.com>
2025-03-12 23:43:09 +0300

    add: echo effect for all sounds

---
+++ Content.Shared/_Scp/Audio/AudioEffectsManagerSystem.cs
@@ -0,0 +1,60 @@
+﻿using Robust.Shared.Audio;
+using Robust.Shared.Audio.Components;
+using Robust.Shared.Audio.Systems;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._Scp.Audio;
+
+// TODO: Разобраться/Смириться с тем, что сервер сайд звуки не поддаются эффектам
+public sealed class AudioEffectsManagerSystem : EntitySystem
+{
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly IPrototypeManager _prototype = default!;
+
+    /// <summary>
+    /// Захешированные эффекты под их прототипами пренитов. Позволяет не засрать слоты OpenAL сотней одинаковых эффектов
+    /// </summary>
+    private static readonly Dictionary<ProtoId<AudioPresetPrototype>, Entity<AudioAuxiliaryComponent>> CachedEffects = new ();
+
+    /// <summary>
+    /// Добавляет переданный эффект к звуку
+    /// </summary>
+    public bool TryAddEffect(Entity<AudioComponent> sound, ProtoId<AudioPresetPrototype> preset)
+    {
+        if (!CachedEffects.TryGetValue(preset, out var effect) && !TryCreateEffect(preset, out effect))
+            return false;
+
+        _audio.SetAuxiliary(sound, sound, effect);
+        return true;
+    }
+
+    /// <summary>
+    /// Пытается создать эффект и захешировать его
+    /// </summary>
+    /// <param name="preset">Пресет эффектов</param>
+    /// <param name="effectStuff">Получаемый эффект. Не представляет собой ничего, когда метод возвращает false</param>
+    /// <returns>Возвращает успешно ли создание и хеширование эффекта</returns>
+    private bool TryCreateEffect(ProtoId<AudioPresetPrototype> preset, out Entity<AudioAuxiliaryComponent> effectStuff)
+    {
+        effectStuff = default;
+
+        if (!_prototype.TryIndex(preset, out var prototype))
+            return false;
+
+        var effect = _audio.CreateEffect();
+        var auxiliary = _audio.CreateAuxiliary();
+
+        _audio.SetEffectPreset(effect.Entity, effect.Component, prototype);
+        _audio.SetEffect(auxiliary.Entity, auxiliary.Component, effect.Entity);
+
+        if (!Exists(auxiliary.Entity))
+            return false;
+
+        if (!CachedEffects.TryAdd(preset, auxiliary))
+            return false;
+
+        effectStuff = auxiliary;
+
+        return true;
+    }
+}
---
+++ Content.Shared/_Scp/Audio/Systems/EchoEffectSystem.cs
@@ -0,0 +1,36 @@
+﻿using Robust.Shared.Audio;
+using Robust.Shared.Audio.Components;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Timing;
+
+namespace Content.Shared._Scp.Audio.Systems;
+
+public sealed class EchoEffectSystem : EntitySystem
+{
+    [Dependency] private readonly AudioEffectsManagerSystem _effectsManager = default!;
+    [Dependency] private readonly IGameTiming _timing = default!;
+
+    private static readonly ProtoId<AudioPresetPrototype> EchoEffectPreset = "Bathroom";
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<AudioComponent, ComponentInit>(OnInit);
+    }
+
+    private void OnInit(Entity<AudioComponent> ent, ref ComponentInit args)
+    {
+        if (!_timing.IsFirstTimePredicted)
+            return;
+
+        if (!Exists(ent))
+            return;
+
+        // Фоновая музыка не должна подвергаться эффектам эха
+        if (ent.Comp.Global)
+            return;
+
+        _effectsManager.TryAddEffect(ent, EchoEffectPreset);
+    }
+}
