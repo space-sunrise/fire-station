83e0e85ac638a8d810c8e7775edf0ee48d83bd1d
drdth <drdtheuser@gmail.com>
2025-03-19 21:37:58 +0300

    fix: prevent shader stacking

--- Content.Client/_Scp/RetroMonitor/RetroMonitorOverlaySystem.cs
+++ Content.Client/_Scp/RetroMonitor/RetroMonitorOverlaySystem.cs
@@ -1,4 +1,5 @@
 ﻿using Content.Client._Scp.Grain;
+using Content.Client._Scp.Vignette;
 using Content.Shared._Scp.RetroMonitor;
 using Robust.Client.Graphics;
 using Robust.Shared.Player;
@@ -8,6 +9,7 @@ namespace Content.Client._Scp.RetroMonitor;
 public sealed class RetroMonitorOverlaySystem : EntitySystem
 {
     [Dependency] private readonly GrainOverlaySystem _grain = default!;
+    [Dependency] private readonly VignetteOverlaySystem _vignette = default!;
     [Dependency] private readonly IOverlayManager _overlayManager = default!;
 
     private readonly RetroMonitorOverlay _overlay = new();
@@ -23,12 +25,16 @@ public sealed class RetroMonitorOverlaySystem : EntitySystem
     private void OnPlayerAttached(Entity<RetroMonitorViewComponent> ent, ref LocalPlayerAttachedEvent args)
     {
         _overlayManager.AddOverlay(_overlay);
+
         _grain.RemoveOverlay();
+        _vignette.RemoveOverlay();
     }
 
     private void OnPlayerDetached(Entity<RetroMonitorViewComponent> ent, ref LocalPlayerDetachedEvent args)
     {
         _overlayManager.RemoveOverlay(_overlay);
+
         _grain.AddOverlay();
+        _vignette.AddOverlay();
     }
 }
