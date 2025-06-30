184e9eddbd17c11ecad0327dbc81189dadc1768c
drdth <drdtheuser@gmail.com>
2025-06-30 16:40:47 +0300

    refactor: ready-to-use grain API

--- Content.Client/_Scp/Shaders/Common/Grain/GrainSystem.cs
+++ Content.Client/_Scp/Shaders/Common/Grain/GrainSystem.cs
@@ -1,12 +1,15 @@
-﻿using Content.Shared._Scp.Other.Shaders;
-using Robust.Shared.Configuration;
+﻿using Robust.Shared.Configuration;
 using Content.Shared._Scp.ScpCCVars;
+using Content.Shared._Scp.Shaders.Grain;
+using Robust.Client.Player;
+using Robust.Shared.Player;
 
 namespace Content.Client._Scp.Shaders.Common.Grain;
 
 public sealed class GrainOverlaySystem : ComponentOverlaySystem<GrainOverlay, GrainOverlayComponent>
 {
     [Dependency] private readonly IConfigurationManager _cfg = default!;
+    [Dependency] private readonly IPlayerManager _player = default!;
 
     public override void Initialize()
     {
@@ -14,20 +17,56 @@ public sealed class GrainOverlaySystem : ComponentOverlaySystem<GrainOverlay, Gr
 
         Overlay = new GrainOverlay();
 
-        _cfg.OnValueChanged(ScpCCVars.GrainToggleOverlay, OnGrainToggleOverlayOptionChanged, true);
+        _cfg.OnValueChanged(ScpCCVars.GrainToggleOverlay, ToggleGrainOverlay);
+        _cfg.OnValueChanged(ScpCCVars.GrainStrength, SetBaseStrength);
     }
 
     public override void Shutdown()
     {
         base.Shutdown();
 
-        _cfg.UnsubValueChanged(ScpCCVars.GrainToggleOverlay, OnGrainToggleOverlayOptionChanged);
+        _cfg.UnsubValueChanged(ScpCCVars.GrainToggleOverlay, ToggleGrainOverlay);
+        _cfg.UnsubValueChanged(ScpCCVars.GrainStrength, SetBaseStrength);
     }
 
-    private void OnGrainToggleOverlayOptionChanged(bool option)
+    protected override void OnPlayerAttached(Entity<GrainOverlayComponent> ent, ref LocalPlayerAttachedEvent args)
+    {
+        base.OnPlayerAttached(ent, ref args);
+
+        SetBaseStrength(_cfg.GetCVar(ScpCCVars.GrainStrength));
+    }
+
+    private void ToggleGrainOverlay(bool option)
     {
         Enabled = option;
 
         ToggleOverlay();
+    }
+
+    private void SetBaseStrength(int value)
+    {
+        var player = _player.LocalEntity;
+
+        if (!player.HasValue)
+            return;
+
+        TrySetBaseStrength(player.Value, value);
+    }
+
+    /// <summary>
+    /// Устанавливает базовую силу шейдера зернистости: <see cref="GrainOverlayComponent.BaseStrength"/>
+    /// </summary>
+    /// <param name="ent">Сущность, к которой будет применено значение</param>
+    /// <param name="value">Значение, которое будет установлено</param>
+    /// <returns>Получилось/Не получилось</returns>
+    public bool TrySetBaseStrength(Entity<GrainOverlayComponent?> ent, int value)
+    {
+        if (!Resolve(ent, ref ent.Comp))
+            return false;
+
+        ent.Comp.BaseStrength = value;
+        Overlay.CurrentStrength = ent.Comp.CurrentStrength;
+
+        return true;
     }
 }
---
+++ Content.Shared/_Scp/Shaders/Grain/GrainOverlayComponent.cs
@@ -0,0 +1,51 @@
+﻿using Content.Shared._Scp.Helpers;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._Scp.Shaders.Grain;
+
+/// <summary>
+/// Компонент, отвечающий за параметры шейдера зернистости.
+/// Наличие компонента необходимо для работы шейдера.
+/// </summary>
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState(true, true)]
+public sealed partial class GrainOverlayComponent : Component
+{
+    /// <summary>
+    /// Максимальные и минимальные значения базовой силы шейдера зернистости.
+    /// Эти пороги используются для настроек клиента и позволяет выбрать доступный диапазон
+    /// </summary>
+    public static readonly MinMaxExtended BaseStrengthLimit = new (70, 140);
+
+    /// <summary>
+    /// Базовая сила зернистости.
+    /// Она определяет то, какой силы будет шейдер при стандартных условиях.
+    /// Может быть настроена клиентом на свое усмотрение.
+    /// </summary>
+    /// <remarks>
+    /// КЛИЕНТСКИЙ ПАРАМЕТР -> НЕ МЕНЯТЬ С СЕРВЕРА!!
+    /// </remarks>
+    [ViewVariables]
+    public int BaseStrength
+    {
+        get => _baseStrength;
+        set => _baseStrength = Math.Clamp(value, BaseStrengthLimit.Min, BaseStrengthLimit.Max);
+    }
+
+    /// <summary>
+    /// Дополнительная сила шейдера зернистости.
+    /// Складывается с <see cref="BaseStrength"/> в <see cref="CurrentStrength"/>.
+    /// Не может быть настроена клиентом, настраивается различными системами извне в качестве негативного эффекта.
+    /// </summary>
+    [AutoNetworkedField, ViewVariables]
+    public int AdditionalStrength;
+
+    /// <summary>
+    /// Текущая сила шейдера зернистости.
+    /// Складывается из <see cref="BaseStrength"/> и <see cref="AdditionalStrength"/>.
+    /// Определяет реальную силу шейдера, учитывая все параметры.
+    /// </summary>
+    [ViewVariables]
+    public int CurrentStrength => BaseStrength + AdditionalStrength;
+
+    private int _baseStrength;
+}
