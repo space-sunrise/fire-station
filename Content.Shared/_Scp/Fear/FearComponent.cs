b9cb545ee252e2ee5b93480a078a0d8447d10b12
drdth <drdtheuser@gmail.com>
2025-07-06 23:43:28 +0300

    add: depending shader strength on fear levels + auto decrease fear level after a while

--- Content.Shared/_Scp/Fear/FearComponent.cs
+++ Content.Shared/_Scp/Fear/FearComponent.cs
@@ -1,22 +1,94 @@
-﻿using Robust.Shared.GameStates;
+﻿using Content.Shared._Scp.Shaders;
+using Robust.Shared.GameStates;
 
 namespace Content.Shared._Scp.Fear;
 
-// TODO: Сделать разные уровни страха при разном приближении
+/// <summary>
+/// Компонент, отвечающий за возможность пугаться.
+/// Обрабатывает уровни страха и хранит текущий.
+/// </summary>
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class FearComponent : Component
 {
+    /// <inheritdoc cref="FearState"/>
     [AutoNetworkedField, ViewVariables]
     public FearState State = FearState.None;
 
-    [ViewVariables]
+    /// <summary>
+    /// Время, через которое уровень страха понизится.
+    /// </summary>
+    [DataField, ViewVariables]
     public TimeSpan TimeToDecreaseFearLevel = TimeSpan.FromMinutes(3f);
+
+    /// <summary>
+    /// Следующее время, когда будут понижен уровень страха со временем.
+    /// </summary>
+    [ViewVariables]
+    public TimeSpan NextTimeDecreaseFearLevel = TimeSpan.Zero;
+
+    #region Shader strength
+
+    /// <summary>
+    /// Этот словарь описывает зависимость силы шейдера зернистости от уровня страха у владельца компонента.
+    /// Эти значения будут суммироваться с другими при добавлении.
+    /// </summary>
+    [ViewVariables]
+    public readonly Dictionary<FearState, float> FearBasedGrainStrength = new ()
+    {
+        { FearState.None , 0f },
+        { FearState.Anxiety, 50f },
+        { FearState.Fear , 100f },
+        { FearState.Terror, 700f },
+    };
+
+    /// <summary>
+    /// Этот словарь описывает зависимость силы шейдера виньетки от уровня страха у владельца компонента.
+    /// Эти значения будут суммироваться с другими при добавлении.
+    /// </summary>
+    [ViewVariables]
+    public readonly Dictionary<FearState, float> FearBasedVignetteStrength = new ()
+    {
+        { FearState.None , 0f },
+        { FearState.Anxiety, 30f },
+        { FearState.Fear , 70f },
+        { FearState.Terror, 400f },
+    };
+
+    /// <summary>
+    /// Хранит текущие параметры силы шейдера, зависимой от уровня страха.
+    /// Каждый шейдер хранится отдельной парой, где ключ компонент шейдера, а значение его fear-based сила.
+    /// </summary>
+    /// <remarks>
+    /// Ключ в виде строки означает имя компонента с силой шейдера.
+    /// </remarks>
+    [ViewVariables]
+    public Dictionary<string, float> CurrentFearBasedShaderStrength = new();
+
+    #endregion
 }
 
+/// <summary>
+/// Уровни страха. Чем больше значение, тем сильнее страх
+/// </summary>
 public enum FearState
 {
+    /// <summary>
+    /// Отсутствие страха. Сущность в спокойном состоянии
+    /// </summary>
     None,
+
+    /// <summary>
+    /// Тревожность. Сущность немного напугана
+    /// </summary>
     Anxiety,
+
+    /// <summary>
+    /// Страх. Сущность испытает прямой страх от чего-либо
+    /// </summary>
     Fear,
+
+    /// <summary>
+    /// Неконтролируемый ужас. Сущность невероятно напугана, что-то СЛИШКОМ ужасно, чтобы знать об этом.
+    /// </summary>
     Terror,
 }
--- Content.Shared/_Scp/Fear/FearSystem.cs
+++ Content.Shared/_Scp/Fear/FearSystem.cs
@@ -3,18 +3,48 @@ using Content.Shared._Scp.Proximity;
 using Content.Shared._Scp.Shaders;
 using Content.Shared._Scp.Shaders.Grain;
 using Content.Shared._Scp.Shaders.Vignette;
+using Robust.Shared.Timing;
 
 namespace Content.Shared._Scp.Fear;
 
-public sealed class FearSystem : EntitySystem
+public sealed partial class FearSystem : EntitySystem
 {
     [Dependency] private readonly SharedShaderStrengthSystem _shaderStrength = default!;
+    [Dependency] private readonly IGameTiming _timing = default!;
 
     public override void Initialize()
     {
         base.Initialize();
 
         SubscribeLocalEvent<FearComponent, ProximityInRangeTargetEvent>(OnProximityInRange);
+        SubscribeLocalEvent<FearComponent, ProximityNotInRangeTargetEvent>(OnProximityNotInRange);
+    }
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        if (!_timing.IsFirstTimePredicted)
+            return;
+
+        var query = EntityQueryEnumerator<FearComponent>();
+
+        // Проходимся по людям с компонентом страха и уменьшаем уровень страха со временем
+        while (query.MoveNext(out var uid, out var fear))
+        {
+            if (fear.State == FearState.None)
+                continue;
+
+            if (fear.NextTimeDecreaseFearLevel > _timing.CurTime)
+                continue;
+
+            var newFearState = GetDecreasedLevel(fear.State);
+
+            if (!TrySetFearLevel((uid, fear), newFearState))
+                continue;
+
+            // TODO: Звук облегчения
+        }
     }
 
     private void OnProximityInRange(Entity<FearComponent> ent, ref ProximityInRangeTargetEvent args)
@@ -24,16 +54,26 @@ public sealed class FearSystem : EntitySystem
         if (!TryComp<FearSourceComponent>(args.Receiver, out var source))
             return;
 
-        // TODO: Какой-то более надежный способ понять, что игрок отошел от источника страха
-        // чем надеяться, что ProximitySystem успеет зарегать расстояние на границе и вызвать ивент
-
-        SetShaderStrength<GrainOverlayComponent>(ent.Owner, args.Range, args.CloseRange, source.GrainShaderStrength);
-        SetShaderStrength<VignetteOverlayComponent>(ent.Owner, args.Range, args.CloseRange, source.VignetteShaderStrength);
-
         TrySetFearLevel(ent.AsNullable(), source.UponComeCloser);
+
+        SetRangeBasedShaderStrength<GrainOverlayComponent>(ent.Owner, args.Range, args.CloseRange, source.GrainShaderStrength);
+        SetRangeBasedShaderStrength<VignetteOverlayComponent>(ent.Owner, args.Range, args.CloseRange, source.VignetteShaderStrength);
     }
 
-    private bool TrySetFearLevel(Entity<FearComponent?> ent, FearState state)
+    private void OnProximityNotInRange(Entity<FearComponent> ent, ref ProximityNotInRangeTargetEvent args)
+    {
+        Logger.Debug($"{Name(ent)}");
+
+        // Как только игрок отходит от источника страха он должен перестать бояться
+        // Но значения шейдера от уровня страха должны продолжать действовать, что и учитывает метод
+        SetShaderStrength<GrainOverlayComponent>(ent.Owner, ent.Comp, 0f);
+        SetShaderStrength<VignetteOverlayComponent>(ent.Owner, ent.Comp, 0f);
+    }
+
+    /// <summary>
+    /// Пытается установить переданный уровень страха.
+    /// </summary>
+    public bool TrySetFearLevel(Entity<FearComponent?> ent, FearState state)
     {
         if (!Resolve(ent, ref ent.Comp))
             return false;
@@ -42,25 +82,62 @@ public sealed class FearSystem : EntitySystem
             return true;
 
         ent.Comp.State = state;
+
+        SetFearBasedShaderStrength((ent, ent.Comp));
+        SetNextCalmDownTime((ent, ent.Comp));
+
         Dirty(ent);
 
         return true;
     }
 
-    private void SetShaderStrength<T>(Entity<T?> ent, float currentRange, float maxRange, MinMaxExtended parameters)
-        where T : IShaderStrength, IComponent
+    /// <summary>
+    /// Устанавливает силу шейдеров в зависимости от текущего уровня страха.
+    /// </summary>
+    private void SetFearBasedShaderStrength(Entity<FearComponent> ent)
     {
-        var strength = CalculateShaderStrength(currentRange, maxRange, parameters);
-        _shaderStrength.TrySetAdditionalStrength(ent, strength);
+        var grainStrength = ent.Comp.FearBasedGrainStrength[ent.Comp.State];
+        var vignetteStrength = ent.Comp.FearBasedVignetteStrength[ent.Comp.State];
+
+        _shaderStrength.TrySetAdditionalStrength<GrainOverlayComponent>(ent.Owner, grainStrength);
+        _shaderStrength.TrySetAdditionalStrength<VignetteOverlayComponent>(ent.Owner, vignetteStrength);
+
+        ent.Comp.CurrentFearBasedShaderStrength[nameof(GrainOverlayComponent)] = grainStrength;
+        ent.Comp.CurrentFearBasedShaderStrength[nameof(VignetteOverlayComponent)] = vignetteStrength;
     }
 
-    private static float CalculateShaderStrength(float currentRange, float maxRange, MinMaxExtended parameters)
+    /// <summary>
+    /// Устанавливает для шейдера параметры силы.
+    /// Сила зависит от расстояния до источника страха и параметров самого источника.
+    /// </summary>
+    /// <param name="ent">Сущность, с компонентами силы шейдера</param>
+    /// <param name="currentRange">Текущее расстояние сущности до источника страха</param>
+    /// <param name="maxRange">Расстояние, на котором начинаются увеличиваться параметры шейдера</param>
+    /// <param name="parameters">Параметры силы шейдера. Минимальное означает силу шейдера на входе в <see cref="maxRange"/>,
+    /// максимальное силу при максимальном приближении к источнику страха</param>
+    /// <param name="fear">Компонент страха</param>
+    /// <typeparam name="T">Компонент с настройками шейдера</typeparam>
+    private void SetRangeBasedShaderStrength<T>(Entity<T?> ent, float currentRange, float maxRange, MinMaxExtended parameters, FearComponent? fear = null)
+        where T : IShaderStrength, IComponent
     {
-        if (currentRange <= 0f)
-            return parameters.Max;
+        if (!Resolve(ent, ref fear))
+            return;
 
-        var proximityFactor = 1f - Math.Clamp(currentRange / maxRange, 0f, 1f);
+        var strength = CalculateShaderStrength(currentRange, maxRange, parameters);
+        SetShaderStrength(ent, fear, strength);
+    }
 
-        return MathHelper.Lerp(parameters.Min, parameters.Max, proximityFactor);
+    /// <summary>
+    /// Устанавливает силу шейдера, учитывая текущий уровень страха и его потребности в силе.
+    /// </summary>
+    /// <typeparam name="T">Компонент настроек, контролирующий силу шейдера</typeparam>
+    public void SetShaderStrength<T>(Entity<T?> ent, FearComponent? fear, float strength)
+        where T : IShaderStrength, IComponent
+    {
+        if (!Resolve(ent, ref fear))
+            return;
+
+        var actualStrength = GetActualStrength<T>(fear, strength);
+        _shaderStrength.TrySetAdditionalStrength(ent, actualStrength);
     }
 }
