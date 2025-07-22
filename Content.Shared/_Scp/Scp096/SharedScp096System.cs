4f824b5a5a247735ea5ebb4feaf2d839656713bd
drdth <drdtheuser@gmail.com>
2025-07-23 01:56:31 +0300

    fix: scp096 angle check

--- Content.Shared/_Scp/Scp096/SharedScp096System.cs
+++ Content.Shared/_Scp/Scp096/SharedScp096System.cs
@@ -281,7 +281,7 @@ public abstract partial class SharedScp096System : EntitySystem
             return false;
 
         // Проверяем, смотри ли цель в лицо 096
-        if (!_fov.IsInViewAngle(viewer, scp.Owner, scp.Comp.ArgoAngle))
+        if (!_fov.IsInViewAngle(viewer, scp.Comp.ArgoAngle, Angle.Zero, scp.Owner))
             return false;
 
         // Соответственно если обе проверки прошли, то цель видит 096
