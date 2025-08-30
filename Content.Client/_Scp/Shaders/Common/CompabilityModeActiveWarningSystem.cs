f7c7fb687741523d5dbb0ced81b868c15eec0a20
drdth <drdtheuser@gmail.com>
2025-08-30 04:53:23 +0300

    add: base window design

---
+++ Content.Client/_Scp/Shaders/Common/CompabilityModeActiveWarningSystem.cs
@@ -0,0 +1,26 @@
+﻿using Content.Client._Scp.UI;
+using Robust.Shared.Configuration;
+using Robust.Shared.Player;
+
+namespace Content.Client._Scp.Shaders.Common;
+
+public sealed class CompabilityModeActiveWarningSystem : EntitySystem
+{
+    [Dependency] private readonly IConfigurationManager _configuration = default!;
+
+    private CompabilityModeActiveWarningWindow? _window;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<LocalPlayerAttachedEvent>(OnPlayerAttached);
+    }
+
+    private void OnPlayerAttached(LocalPlayerAttachedEvent args)
+    {
+        _window?.Dispose();
+        _window = new CompabilityModeActiveWarningWindow();
+        _window.OpenCentered();
+    }
+}
