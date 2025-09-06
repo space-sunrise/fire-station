7983edf2110d1f6a90ff17bbf3e92b4997fc6d9c
drdth <drdtheuser@gmail.com>
2025-09-06 18:23:05 +0300

    add: blind effect for debugging

---
+++ Content.Shared/_Scp/Trigger/BlindOnTrigger/BlindOnTriggerComponent.cs
@@ -0,0 +1,10 @@
+﻿using Content.Shared.Trigger.Components.Effects;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._Scp.Trigger.BlindOnTrigger;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class BlindOnTriggerComponent : BaseXOnTriggerComponent
+{
+    public TimeSpan Time = TimeSpan.FromSeconds(7);
+}
---
+++ Content.Shared/_Scp/Trigger/BlindOnTrigger/BlindOnTriggerSystem.cs
@@ -0,0 +1,24 @@
+﻿using Content.Shared._Scp.Scp173;
+using Content.Shared.Trigger;
+
+namespace Content.Shared._Scp.Trigger.BlindOnTrigger;
+
+public sealed class BlindOnTriggerSystem : EntitySystem
+{
+    [Dependency] private readonly SharedScp173System _scp173 = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<BlindOnTriggerComponent, TriggerEvent>(OnTrigger);
+    }
+
+    private void OnTrigger(Entity<BlindOnTriggerComponent> ent, ref TriggerEvent args)
+    {
+        if (args.Key != null && !ent.Comp.KeysIn.Contains(args.Key))
+            return;
+
+        _scp173.BlindEveryoneInRange(ent, ent.Comp.Time, false);
+    }
+}
