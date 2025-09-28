73b9d9c57eb8cebcaeea7b00ea0d329a4cfca729
drdth <drdtheuser@gmail.com>
2025-09-28 21:06:01 +0300

    fix: ai review

--- Content.Server/_Scp/Other/RandomPlanet/RandomPlanetComponent.cs
+++ Content.Server/_Scp/Other/RandomPlanet/RandomPlanetComponent.cs
@@ -1,7 +1,6 @@
 ﻿using Content.Shared.Parallax.Biomes;
 using Content.Shared.Weather;
 using Robust.Shared.Prototypes;
-using Robust.Shared.Serialization;
 
 namespace Content.Server._Scp.Other.RandomPlanet;
 
@@ -11,14 +10,39 @@ namespace Content.Server._Scp.Other.RandomPlanet;
 [RegisterComponent]
 public sealed partial class RandomPlanetComponent : Component
 {
+    /// <summary>
+    /// <see cref="StationPlanetData"/>
+    /// </summary>
     [DataField(required: true)]
     public List<StationPlanetData> Data = [];
 }
 
+/// <summary>
+/// Данные, нужные для генерации биома и погоды у комплекса.
+/// Хранятся в качестве отдельной структуры для удобства работы и сериализации.
+/// </summary>
 [DataDefinition, Serializable]
 public sealed partial class StationPlanetData
 {
-    [DataField] public ProtoId<BiomeTemplatePrototype> Biome;
-    [DataField] public List<ProtoId<WeatherPrototype>>? Weather;
-    [DataField] public List<int>? Seed;
+    /// <summary>
+    /// Прототип биома, который будет использован.
+    /// </summary>
+    [DataField(required: true)]
+    public ProtoId<BiomeTemplatePrototype> Biome;
+
+    /// <summary>
+    /// Прототипы погоды, которые могут быть использованы.
+    /// Если ничего не указано - погоды не будет.
+    /// Если указано будет выбрана случайная погода
+    /// </summary>
+    [DataField]
+    public List<ProtoId<WeatherPrototype>>? Weather;
+
+    /// <summary>
+    /// Зерна генерации биомов.
+    /// Если не задано будет выбрано случайное.
+    /// Если задано - случайное из списка.
+    /// </summary>
+    [DataField]
+    public List<int>? Seed;
 }
