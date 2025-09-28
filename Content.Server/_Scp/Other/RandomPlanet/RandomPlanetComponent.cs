39537ae103fe87a78837afef44114491304dcbb7
drdth <drdtheuser@gmail.com>
2025-09-28 19:03:17 +0300

    add: random planet settings component with weather and seed support

---
+++ Content.Server/_Scp/Other/RandomPlanet/RandomPlanetComponent.cs
@@ -0,0 +1,24 @@
+﻿using Content.Shared.Parallax.Biomes;
+using Content.Shared.Weather;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
+
+namespace Content.Server._Scp.Other.RandomPlanet;
+
+/// <summary>
+/// Компонент, создающий на станции случайный биом из списка.
+/// </summary>
+[RegisterComponent]
+public sealed partial class RandomPlanetComponent : Component
+{
+    [DataField(required: true)]
+    public List<StationPlanetData> Data = [];
+}
+
+[DataDefinition, Serializable, NetSerializable]
+public sealed partial class StationPlanetData
+{
+    [DataField] public ProtoId<BiomeTemplatePrototype> Biome;
+    [DataField] public List<ProtoId<WeatherPrototype>>? Weather;
+    [DataField] public List<int>? Seed;
+}
