4e099634bb6c51709e5fc1952abd51c7b4b77035
drdth <drdtheuser@gmail.com>
2025-10-02 04:37:31 +0300

    tweak: reduce save percent

--- Content.Server/_Scp/MetaGarbage/MetaGarbageTargetComponent.cs
+++ Content.Server/_Scp/MetaGarbage/MetaGarbageTargetComponent.cs
@@ -17,7 +17,7 @@ public sealed partial class MetaGarbageTargetComponent : Component
     /// Какое процентное соотношение от общего числа собранного в прошлом раунде мусора вернется в новом раунде?
     /// </summary>
     [DataField]
-    public float SpawnPercent = 100f;
+    public float SpawnPercent = 70f;
 
     /// <summary>
     /// Модификаторы количества луж/следов с заданными реагентами.
