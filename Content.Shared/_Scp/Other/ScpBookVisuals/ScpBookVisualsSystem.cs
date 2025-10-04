6c26c3d17f08e076e856d2bae17ab4dd890e1f65
ThereDrD <88589686+ThereDrD0@users.noreply.github.com>
2025-10-04 16:13:14 +0300

    Дневник чумного доктора и плюшевый 999 (#608)

---
+++ Content.Shared/_Scp/Other/ScpBookVisuals/ScpBookVisualsSystem.cs
@@ -0,0 +1,45 @@
+﻿using Content.Shared.Paper;
+
+namespace Content.Shared._Scp.Other.ScpBookVisuals;
+
+public sealed class ScpBookVisualsSystem : EntitySystem
+{
+    [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
+    [Dependency] private readonly SharedUserInterfaceSystem _ui = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<ScpBookVisualsComponent, BoundUIOpenedEvent>(OnOpen);
+        SubscribeLocalEvent<ScpBookVisualsComponent, BoundUIClosedEvent>(OnClose);
+        SubscribeLocalEvent<ScpBookVisualsComponent, PaperWrittenEvent>(OnWritten);
+    }
+
+    private void OnOpen(Entity<ScpBookVisualsComponent> ent, ref BoundUIOpenedEvent args)
+    {
+        UpdateOpenState(ent);
+    }
+
+    private void OnClose(Entity<ScpBookVisualsComponent> ent, ref BoundUIClosedEvent args)
+    {
+        _appearance.SetData(ent, ScpBookVisualLayers.ScpBookVisualState, ScpBookVisualState.Closed);
+    }
+
+    private void OnWritten(Entity<ScpBookVisualsComponent> ent, ref PaperWrittenEvent args)
+    {
+        UpdateOpenState(ent);
+    }
+
+    private void UpdateOpenState(EntityUid ent)
+    {
+        if (!_ui.IsUiOpen(ent, PaperComponent.PaperUiKey.Key))
+            return;
+
+        var state = TryComp<PaperComponent>(ent, out var paper) && !string.IsNullOrEmpty(paper.Content)
+            ? ScpBookVisualState.OpenWritten
+            : ScpBookVisualState.Open;
+
+        _appearance.SetData(ent, ScpBookVisualLayers.ScpBookVisualState, state);
+    }
+}
