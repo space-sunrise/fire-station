EmoGarbage404/ephemeral-space

cone man 2 (clown man 2 reference) (#173)
* hold to face

* naive lag

* yaaay correct solutions feel good to implement

* a

Content.Client/_ES/Viewcone/ESViewconeOverlayManagementSystem.cs
@@ -1,8 +1,12 @@
 using Content.Client._ES.Viewcone.Overlays;
+using Content.Client.Eye;
 using Content.Shared._ES.Viewcone;
+using Content.Shared.MouseRotator;
 using Robust.Client.GameObjects;
 using Robust.Client.Graphics;
+using Robust.Client.Input;
 using Robust.Client.Player;
+using Robust.Shared.Map;
 using Robust.Shared.Physics;
 using Robust.Shared.Player;
 
@@ -10,14 +14,20 @@ namespace Content.Client._ES.Viewcone;
 
 /// <summary>
 ///     Handles adding and removing the viewcone overlays, as well as ferrying data between them
+///     Also handles calculating desired view angle for active viewcones so overlays can use it
 /// </summary>
 public sealed class ESViewconeOverlayManagementSystem : EntitySystem
 {
     [Dependency] private readonly IPlayerManager _playerManager = default!;
     [Dependency] private readonly IOverlayManager _overlayMan = default!;
+    [Dependency] private readonly IInputManager _input = default!;
+    [Dependency] private readonly IEyeManager _eye = default!;
+    [Dependency] private readonly SharedTransformSystem _xform = default!;
     private ESViewconeConeOverlay _coneOverlay = default!;
     private ESViewconeSetAlphaOverlay _setAlphaOverlay = default!;
     private ESViewconeResetAlphaOverlay _resetAlphaOverlay = default!;
+
+    private const float LerpHalfLife = 0.05f;
 
     // slightly balls state management, but
     // done so we don't have to requery within the same frame
@@ -41,6 +51,63 @@ public sealed class ESViewconeOverlayManagementSystem : EntitySystem
         _coneOverlay = new();
         _setAlphaOverlay = new();
         _resetAlphaOverlay = new();
+    }
+
+    public override void FrameUpdate(float frameTime)
+    {
+        base.FrameUpdate(frameTime);
+
+        // the reason we use lerpingeye here in the query first is to
+        // specifically check for eyes that we are actually rendering (lerpingeye already handles this sort of
+        // its like jank as fuck in that system but whatever thats like not my problem )
+        var enumerator = AllEntityQuery<LerpingEyeComponent, EyeComponent, ESViewconeComponent, TransformComponent>();
+        while (enumerator.MoveNext(out var uid, out _, out var eye, out var viewcone, out var xform))
+        {
+            var eyeAngle = eye.Rotation;
+            var rotation = _xform.GetWorldRotation(xform);
+            var playerAngle = rotation;
+            var desiredWasNull = viewcone.DesiredViewAngle == null;
+
+            if (HasComp<MouseRotatorComponent>(uid))
+            {
+                var mousePos = _eye.PixelToMap(_input.MouseScreenPosition);
+                if (mousePos.MapId != MapId.Nullspace)
+                    playerAngle = (mousePos.Position - _xform.GetMapCoordinates(xform).Position).ToAngle() + Angle.FromDegrees(90);
+
+                viewcone.LastMouseRotationAngle = playerAngle;
+            }
+            else if (viewcone.LastMouseRotationAngle != 0f)
+            {
+                // if last frame we had a mouse rotation angle, but now we dont,
+                // that means it was disabled
+                // but, we should keep the old mouse angle for viewcone, at least until the real angle actually changes
+                if (MathHelper.CloseToPercent(viewcone.LastWorldRotationAngle, playerAngle, .000001d))
+                {
+                    playerAngle = viewcone.LastMouseRotationAngle;
+                }
+                else
+                {
+                    viewcone.LastMouseRotationAngle = 0f;
+                }
+            }
+
+            viewcone.LastWorldRotationAngle = rotation;
+            viewcone.DesiredViewAngle = playerAngle + eyeAngle;
+
+            // if desired angle was null before we set it
+            // then just set viewangle to it immediately
+            // (assume it was first frame)
+            if (desiredWasNull)
+            {
+                viewcone.ViewAngle = viewcone.DesiredViewAngle.Value;
+                continue;
+            }
+
+            // framerate-independent lerp
+            // https://twitter.com/FreyaHolmer/status/1757836988495847568
+            // convert to angle first so we lerp thru shortestdistance
+            viewcone.ViewAngle = Angle.Lerp(viewcone.ViewAngle, viewcone.DesiredViewAngle.Value, 1f - MathF.Pow(2f, -(frameTime / LerpHalfLife)));
+        }
     }
 
     private void OnPlayerAttached(Entity<ESViewconeComponent> entity, ref LocalPlayerAttachedEvent args)

Content.Client/_ES/Viewcone/Overlays/ESViewconeConeOverlay.cs
@@ -1,3 +1,4 @@
+using Content.Client.Eye;
 using Content.Shared._ES.Viewcone;
 using Content.Shared.MouseRotator;
 using Robust.Client.Graphics;
@@ -6,6 +7,7 @@ using Robust.Client.Player;
 using Robust.Shared.Enums;
 using Robust.Shared.Map;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Timing;
 
 namespace Content.Client._ES.Viewcone.Overlays;
 
@@ -15,10 +17,7 @@ namespace Content.Client._ES.Viewcone.Overlays;
 public sealed class ESViewconeConeOverlay : Overlay
 {
     [Dependency] private readonly IEntityManager _ent = default!;
-    [Dependency] private readonly IInputManager _input = default!;
-    [Dependency] private readonly IEyeManager _eye = default!;
     [Dependency] private readonly IPrototypeManager _proto = default!;
-    private readonly SharedTransformSystem _xform;
 
     public override OverlaySpace Space => OverlaySpace.WorldSpace;
     public override bool RequestScreenTexture => true;
@@ -26,7 +25,7 @@ public sealed class ESViewconeConeOverlay : Overlay
     public static ProtoId<ShaderPrototype> ShaderPrototype = "Viewcone";
     private readonly ShaderInstance _viewconeShader;
 
-    private Entity<EyeComponent, TransformComponent>? _eyeEntity;
+    private Entity<EyeComponent, ESViewconeComponent, TransformComponent>? _eyeEntity;
     private float _coneAngle;
     private float _coneFeather;
     private float _coneIgnoreRadius;
@@ -35,7 +34,6 @@ public sealed class ESViewconeConeOverlay : Overlay
     public ESViewconeConeOverlay()
     {
         IoCManager.InjectDependencies(this);
-        _xform = _ent.System<SharedTransformSystem>();
         _viewconeShader = _proto.Index(ShaderPrototype).InstanceUnique();
     }
 
@@ -44,9 +42,11 @@ public sealed class ESViewconeConeOverlay : Overlay
         _eyeEntity = null;
 
         // This is really stupid but there isn't another way to reverse an eye entity from just an IEye afaict
-        // It's not really inefficient though. theres barely any of those fuckin things anyway (? verify that) (maybe this scales with players in view) (shit)
-        var enumerator = _ent.AllEntityQueryEnumerator<EyeComponent, ESViewconeComponent, TransformComponent>();
-        while (enumerator.MoveNext(out var uid, out var eye, out var viewcone, out var xform))
+        // It's not really inefficient though. theres barely any of those fuckin things anyway
+        // lerpingeye used because that system already does the busywork of figuring out which eyes are 'rendering' sort of
+        // so we dont have to query other players eyes (probably barely makes a difference anyway)
+        var enumerator = _ent.AllEntityQueryEnumerator<LerpingEyeComponent, EyeComponent, ESViewconeComponent, TransformComponent>();
+        while (enumerator.MoveNext(out var uid, out var _, out var eye, out var viewcone, out var xform))
         {
             if (args.Viewport.Eye != eye.Eye)
                 continue;
@@ -55,7 +55,7 @@ public sealed class ESViewconeConeOverlay : Overlay
             _coneFeather = viewcone.ConeFeather;
             _coneIgnoreRadius = (viewcone.ConeIgnoreRadius - viewcone.ConeIgnoreFeather) * 50f;
             _coneIgnoreFeather = Math.Max(viewcone.ConeIgnoreFeather * 200f, 8f);
-            _eyeEntity = (uid, eye, xform);
+            _eyeEntity = (uid, eye, viewcone, xform);
             break;
         }
 
@@ -70,22 +70,9 @@ public sealed class ESViewconeConeOverlay : Overlay
         var worldHandle = args.WorldHandle;
         var viewport = args.WorldBounds;
 
-        var zoom = _eyeEntity.Value.Comp1.Zoom.X;
-        var eyeAngle = (float) _eyeEntity.Value.Comp1.Rotation.Theta;
-        var playerAngle = (float) _xform.GetWorldRotation(_eyeEntity.Value.Comp2).Theta;
-
-        if (_ent.HasComponent<MouseRotatorComponent>(_eyeEntity))
-        {
-            var mousePos = _eye.PixelToMap(_input.MouseScreenPosition);
-            if (mousePos.MapId != MapId.Nullspace)
-                playerAngle = (float) (mousePos.Position - _xform.GetMapCoordinates(_eyeEntity.Value).Position).ToAngle().Theta + MathHelper.DegreesToRadians(90f);
-        }
-
-        var viewAngle = playerAngle + eyeAngle;
-
         _viewconeShader.SetParameter("SCREEN_TEXTURE", ScreenTexture);
-        _viewconeShader.SetParameter("Zoom", zoom);
-        _viewconeShader.SetParameter("ViewAngle", viewAngle);
+        _viewconeShader.SetParameter("Zoom", _eyeEntity.Value.Comp1.Zoom.X);
+        _viewconeShader.SetParameter("ViewAngle", (float) _eyeEntity.Value.Comp2.ViewAngle.Theta);
         _viewconeShader.SetParameter("ConeAngle", _coneAngle);
         _viewconeShader.SetParameter("ConeFeather", _coneFeather);
         _viewconeShader.SetParameter("ConeIgnoreRadius", _coneIgnoreRadius);

Content.Client/_ES/Viewcone/Overlays/ESViewconeSetAlphaOverlay.cs
@@ -18,8 +18,6 @@ namespace Content.Client._ES.Viewcone.Overlays;
 public sealed class ESViewconeSetAlphaOverlay : Overlay
 {
     [Dependency] private readonly IEntityManager _ent = default!;
-    [Dependency] private readonly IEyeManager _eye = default!;
-    [Dependency] private readonly IInputManager _input = default!;
     private readonly ESViewconeOverlayManagementSystem _cone;
     private readonly ESViewconeOccludableTreeSystem _tree;
     private readonly TransformSystem _xform;
@@ -70,26 +68,12 @@ public sealed class ESViewconeSetAlphaOverlay : Overlay
         var (ent, eye, cone) = _nextEye.Value;
 
         var eyeTransform = _ent.GetComponent<TransformComponent>(ent);
-        var (eyePos, eyeRot) = _xform.GetWorldPositionRotation(eyeTransform);
+        var eyePos = _xform.GetWorldPosition(eyeTransform);
+        var eyeRot = cone.ViewAngle - eye.Rotation; // subtract rotation cuz idk. the lerp adds it but this doesnt want it for some reason idk.
 
         // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         // !! Thank You Bhijn God (TYBG) for 95% of the rest of this methods code !!
         // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-        if (_ent.HasComponent<MouseRotatorComponent>(ent))
-        {
-            // this should work for multiviewport. at least, about as well as people will expect
-            // this wont run for other eye entities that have viewcones
-            // (if any even end up existing.. I probably made all this code viewport agnostic for no reason)
-            // (but it'd be nice to have cameras that have viewcones. right. right)
-            // (Withers )
-            // but for a separate viewport following the same mouserotator entity, idk, it probably works fine.
-            // when is that even going to happen.
-            var mousePos = _eye.PixelToMap(_input.MouseScreenPosition);
-            if (mousePos.MapId == eyeTransform.MapID)
-                eyeRot = (mousePos.Position - _xform.GetMapCoordinates(eyeTransform).Position).ToWorldAngle();
-        }
-
         var radConeAngle = MathHelper.DegreesToRadians(cone.ConeAngle);
         var radConeFeather = MathHelper.DegreesToRadians(cone.ConeFeather);
 

space-sunrise/fire-station

Content.Client/_ES/Viewcone/ESViewconeOverlayManagementSystem.cs
@@ -1,22 +1,34 @@
 using Content.Client._ES.Viewcone.Overlays;
+using Content.Client.Eye;
+using Content.Shared._ES.Viewcone;
 using Content.Shared._Scp.Watching.FOV;
+using Content.Shared.MouseRotator;
 using Robust.Client.GameObjects;
 using Robust.Client.Graphics;
+using Robust.Client.Input;
 using Robust.Client.Player;
+using Robust.Shared.Map;
+using Robust.Shared.Physics;
 using Robust.Shared.Player;
 
 namespace Content.Client._ES.Viewcone;
 
 /// <summary>
 ///     Handles adding and removing the viewcone overlays, as well as ferrying data between them
+///     Also handles calculating desired view angle for active viewcones so overlays can use it
 /// </summary>
 public sealed class ESViewconeOverlayManagementSystem : EntitySystem
 {
     [Dependency] private readonly IPlayerManager _playerManager = default!;
     [Dependency] private readonly IOverlayManager _overlayMan = default!;
+    [Dependency] private readonly IInputManager _input = default!;
+    [Dependency] private readonly IEyeManager _eye = default!;
+    [Dependency] private readonly SharedTransformSystem _xform = default!;
     private ESViewconeConeOverlay _coneOverlay = default!;
     private ESViewconeSetAlphaOverlay _setAlphaOverlay = default!;
     private ESViewconeResetAlphaOverlay _resetAlphaOverlay = default!;
+
+    private const float LerpHalfLife = 0.05f;
 
     // slightly balls state management, but
     // done so we don't have to requery within the same frame
@@ -42,7 +54,65 @@ public sealed class ESViewconeOverlayManagementSystem : EntitySystem
         _resetAlphaOverlay = new();
     }
 
-    private void OnPlayerAttached(Entity<FieldOfViewComponent> ent, ref LocalPlayerAttachedEvent args)
+
+    public override void FrameUpdate(float frameTime)
+    {
+        base.FrameUpdate(frameTime);
+
+        // the reason we use lerpingeye here in the query first is to
+        // specifically check for eyes that we are actually rendering (lerpingeye already handles this sort of
+        // its like jank as fuck in that system but whatever thats like not my problem )
+        var enumerator = AllEntityQuery<LerpingEyeComponent, EyeComponent, FieldOfViewComponent, TransformComponent>();
+        while (enumerator.MoveNext(out var uid, out _, out var eye, out var viewcone, out var xform))
+        {
+            var eyeAngle = eye.Rotation;
+            var rotation = _xform.GetWorldRotation(xform);
+            var playerAngle = rotation;
+            var desiredWasNull = viewcone.DesiredViewAngle == null;
+
+            if (HasComp<MouseRotatorComponent>(uid))
+            {
+                var mousePos = _eye.PixelToMap(_input.MouseScreenPosition);
+                if (mousePos.MapId != MapId.Nullspace)
+                    playerAngle = (mousePos.Position - _xform.GetMapCoordinates(xform).Position).ToAngle() + Angle.FromDegrees(90);
+
+                viewcone.LastMouseRotationAngle = playerAngle;
+            }
+            else if (viewcone.LastMouseRotationAngle != 0f)
+            {
+                // if last frame we had a mouse rotation angle, but now we dont,
+                // that means it was disabled
+                // but, we should keep the old mouse angle for viewcone, at least until the real angle actually changes
+                if (MathHelper.CloseToPercent(viewcone.LastWorldRotationAngle, playerAngle, .000001d))
+                {
+                    playerAngle = viewcone.LastMouseRotationAngle;
+                }
+                else
+                {
+                    viewcone.LastMouseRotationAngle = 0f;
+                }
+            }
+
+            viewcone.LastWorldRotationAngle = rotation;
+            viewcone.DesiredViewAngle = playerAngle + eyeAngle;
+
+            // if desired angle was null before we set it
+            // then just set viewangle to it immediately
+            // (assume it was first frame)
+            if (desiredWasNull)
+            {
+                viewcone.ViewAngle = viewcone.DesiredViewAngle.Value;
+                continue;
+            }
+
+            // framerate-independent lerp
+            // https://twitter.com/FreyaHolmer/status/1757836988495847568
+            // convert to angle first so we lerp thru shortestdistance
+            viewcone.ViewAngle = Angle.Lerp(viewcone.ViewAngle, viewcone.DesiredViewAngle.Value, 1f - MathF.Pow(2f, -(frameTime / LerpHalfLife)));
+        }
+    }
+
+    private void OnPlayerAttached(Entity<ESViewconeComponent> entity, ref LocalPlayerAttachedEvent args)
     {
         AddOverlays();
     }

Content.Client/_ES/Viewcone/Overlays/ESViewconeConeOverlay.cs
@@ -1,3 +1,5 @@
+using Content.Client.Eye;
+using Content.Shared._ES.Viewcone;
 using Content.Shared._Scp.Watching.FOV;
 using Content.Shared.MouseRotator;
 using Robust.Client.Graphics;
@@ -5,6 +7,7 @@ using Robust.Client.Input;
 using Robust.Shared.Enums;
 using Robust.Shared.Map;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Timing;
 
 namespace Content.Client._ES.Viewcone.Overlays;
 
@@ -14,10 +17,7 @@ namespace Content.Client._ES.Viewcone.Overlays;
 public sealed class ESViewconeConeOverlay : Overlay
 {
     [Dependency] private readonly IEntityManager _ent = default!;
-    [Dependency] private readonly IInputManager _input = default!;
-    [Dependency] private readonly IEyeManager _eye = default!;
     [Dependency] private readonly IPrototypeManager _proto = default!;
-    private readonly SharedTransformSystem _xform;
 
     public override OverlaySpace Space => OverlaySpace.WorldSpace;
     public override bool RequestScreenTexture => true;
@@ -25,7 +25,7 @@ public sealed class ESViewconeConeOverlay : Overlay
     private static readonly ProtoId<ShaderPrototype> ShaderPrototype = "Viewcone";
     private readonly ShaderInstance _viewconeShader;
 
-    private Entity<EyeComponent, TransformComponent>? _eyeEntity;
+    private Entity<EyeComponent, FieldOfViewComponent, TransformComponent>? _eyeEntity;
     private float _coneAngle;
     private float _coneFeather;
     private float _coneIgnoreRadius;
@@ -34,7 +34,6 @@ public sealed class ESViewconeConeOverlay : Overlay
     public ESViewconeConeOverlay()
     {
         IoCManager.InjectDependencies(this);
-        _xform = _ent.System<SharedTransformSystem>();
         _viewconeShader = _proto.Index(ShaderPrototype).InstanceUnique();
     }
 
@@ -43,9 +42,11 @@ public sealed class ESViewconeConeOverlay : Overlay
         _eyeEntity = null;
 
         // This is really stupid but there isn't another way to reverse an eye entity from just an IEye afaict
-        // It's not really inefficient though. theres barely any of those fuckin things anyway (? verify that) (maybe this scales with players in view) (shit)
-        var enumerator = _ent.AllEntityQueryEnumerator<EyeComponent, FieldOfViewComponent, TransformComponent>();
-        while (enumerator.MoveNext(out var uid, out var eye, out var viewcone, out var xform))
+        // It's not really inefficient though. theres barely any of those fuckin things anyway
+        // lerpingeye used because that system already does the busywork of figuring out which eyes are 'rendering' sort of
+        // so we dont have to query other players eyes (probably barely makes a difference anyway)
+        var enumerator = _ent.AllEntityQueryEnumerator<LerpingEyeComponent, EyeComponent, FieldOfViewComponent, TransformComponent>();
+        while (enumerator.MoveNext(out var uid, out var _, out var eye, out var viewcone, out var xform))
         {
             if (args.Viewport.Eye != eye.Eye)
                 continue;
@@ -54,7 +55,7 @@ public sealed class ESViewconeConeOverlay : Overlay
             _coneFeather = viewcone.AngleTolerance;
             _coneIgnoreRadius = (viewcone.ConeIgnoreRadius - viewcone.ConeIgnoreFeather) * 50f;
             _coneIgnoreFeather = Math.Max(viewcone.ConeIgnoreFeather * 200f, 8f);
-            _eyeEntity = (uid, eye, xform);
+            _eyeEntity = (uid, eye, viewcone, xform);
             break;
         }
 
@@ -69,22 +70,9 @@ public sealed class ESViewconeConeOverlay : Overlay
         var worldHandle = args.WorldHandle;
         var viewport = args.WorldBounds;
 
-        var zoom = _eyeEntity.Value.Comp1.Zoom.X;
-        var eyeAngle = (float) _eyeEntity.Value.Comp1.Rotation.Theta;
-        var playerAngle = (float) _xform.GetWorldRotation(_eyeEntity.Value.Comp2).Theta;
-
-        if (_ent.HasComponent<MouseRotatorComponent>(_eyeEntity))
-        {
-            var mousePos = _eye.PixelToMap(_input.MouseScreenPosition);
-            if (mousePos.MapId != MapId.Nullspace)
-                playerAngle = (float) (mousePos.Position - _xform.GetMapCoordinates(_eyeEntity.Value).Position).ToAngle().Theta + MathHelper.DegreesToRadians(90f);
-        }
-
-        var viewAngle = playerAngle + eyeAngle;
-
         _viewconeShader.SetParameter("SCREEN_TEXTURE", ScreenTexture);
-        _viewconeShader.SetParameter("Zoom", zoom);
-        _viewconeShader.SetParameter("ViewAngle", viewAngle);
+        _viewconeShader.SetParameter("Zoom", _eyeEntity.Value.Comp1.Zoom.X);
+        _viewconeShader.SetParameter("ViewAngle", (float) _eyeEntity.Value.Comp2.ViewAngle.Theta);
         _viewconeShader.SetParameter("ConeAngle", _coneAngle);
         _viewconeShader.SetParameter("ConeFeather", _coneFeather);
         _viewconeShader.SetParameter("ConeIgnoreRadius", _coneIgnoreRadius);

Content.Client/_ES/Viewcone/Overlays/ESViewconeSetAlphaOverlay.cs
@@ -17,8 +17,6 @@ namespace Content.Client._ES.Viewcone.Overlays;
 public sealed class ESViewconeSetAlphaOverlay : Overlay
 {
     [Dependency] private readonly IEntityManager _ent = default!;
-    [Dependency] private readonly IEyeManager _eye = default!;
-    [Dependency] private readonly IInputManager _input = default!;
     private readonly ESViewconeOverlayManagementSystem _cone;
     private readonly ESViewconeOccludableTreeSystem _tree;
     private readonly TransformSystem _xform;
@@ -69,26 +67,12 @@ public sealed class ESViewconeSetAlphaOverlay : Overlay
         var (ent, eye, cone) = _nextEye.Value;
 
         var eyeTransform = _ent.GetComponent<TransformComponent>(ent);
-        var (eyePos, eyeRot) = _xform.GetWorldPositionRotation(eyeTransform);
+        var eyePos = _xform.GetWorldPosition(eyeTransform);
+        var eyeRot = cone.Angle - eye.Rotation; // subtract rotation cuz idk. the lerp adds it but this doesnt want it for some reason idk.
 
         // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         // !! Thank You Bhijn God (TYBG) for 95% of the rest of this methods code !!
         // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-        if (_ent.HasComponent<MouseRotatorComponent>(ent))
-        {
-            // this should work for multiviewport. at least, about as well as people will expect
-            // this wont run for other eye entities that have viewcones
-            // (if any even end up existing.. I probably made all this code viewport agnostic for no reason)
-            // (but it'd be nice to have cameras that have viewcones. right. right)
-            // (Withers )
-            // but for a separate viewport following the same mouserotator entity, idk, it probably works fine.
-            // when is that even going to happen.
-            var mousePos = _eye.PixelToMap(_input.MouseScreenPosition);
-            if (mousePos.MapId == eyeTransform.MapID)
-                eyeRot = (mousePos.Position - _xform.GetMapCoordinates(eyeTransform).Position).ToWorldAngle();
-        }
-
         var radConeAngle = MathHelper.DegreesToRadians(cone.Angle);
         var radConeFeather = MathHelper.DegreesToRadians(cone.AngleTolerance);
 