585dfcad1c57f10785092baa27b6d28f243d26e8
Neyran <96446770+NeyranDis@users.noreply.github.com>
2025-10-10 01:12:31 +0300

    Update Content.Server/_Scp/ComplexElevator/ComplexElevatorSystem.cs
    Co-authored-by: ThereDrD <88589686+ThereDrD0@users.noreply.github.com>

--- Content.Server/_Scp/ComplexElevator/ComplexElevatorSystem.cs
+++ Content.Server/_Scp/ComplexElevator/ComplexElevatorSystem.cs
@@ -296,7 +296,8 @@ public sealed class ComplexElevatorSystem : EntitySystem
         var query = EntityQueryEnumerator<ElevatorDoorComponent>();
         while (query.MoveNext(out var doorUid, out var doorComp))
         {
-            if (!(doorComp.ElevatorId == elevatorId && doorComp.Floor == floor)) continue;
+            if (doorComp.ElevatorId != elevatorId || doorComp.Floor != floor)
+                continue;
             if (!_doorSystem.TryClose(doorUid))
                 return false;
             else
