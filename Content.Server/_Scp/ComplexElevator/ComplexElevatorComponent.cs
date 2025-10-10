22689acfd7b6cd5d22c6aab5fb53a9f983dbf7ea
Neyran <96446770+NeyranDis@users.noreply.github.com>
2025-10-11 00:46:05 +0300

    Обновление лифта & Фиксы (#616)
    Co-authored-by: ThereDrD <88589686+ThereDrD0@users.noreply.github.com>

--- Content.Server/_Scp/ComplexElevator/ComplexElevatorComponent.cs
+++ Content.Server/_Scp/ComplexElevator/ComplexElevatorComponent.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using Robust.Shared.Audio;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Maths;
 
 namespace Content.Server._Scp.ComplexElevator;
 
@@ -25,6 +27,9 @@ public sealed partial class ComplexElevatorComponent : Component
     public TimeSpan IntermediateDelay = TimeSpan.FromSeconds(6);
 
     [DataField]
+    public TimeSpan DoorCloseDelay = TimeSpan.FromSeconds(0.3);
+
+    [DataField]
     public SoundSpecifier StartSound = new SoundPathSpecifier("/Audio/_Scp/Machines/Elevator/ElevatorClose.ogg");
 
     [DataField]
@@ -32,6 +37,9 @@ public sealed partial class ComplexElevatorComponent : Component
 
     [DataField]
     public SoundSpecifier ArrivalSound = new SoundPathSpecifier("/Audio/_Scp/Machines/Elevator/Beep-elevator.ogg");
+
+    [DataField]
+    public float DoorBlockCheckRange = 0.6f;
 
     public bool IsMoving = false;
 }
