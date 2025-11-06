3a4bf2aaae31dc98c580b024b55c261d072c6bbb
ThereDrD <88589686+ThereDrD0@users.noreply.github.com>
2025-11-06 06:41:54 +0300

    Убрал коллизии у мелких мобов (#656)

---
+++ Content.Server/_Scp/Other/NoSmallMobsCollision/NoSmallMobsCollisionSystem.cs
@@ -0,0 +1,34 @@
+﻿using Content.Shared.Movement.Components;
+using Content.Shared.Physics;
+using Robust.Shared.Physics.Components;
+
+namespace Content.Server._Scp.Other.NoSmallMobsCollision;
+
+/// <summary>
+/// Система для удаления коллизии у мелких мобов, так как она им не нужна.
+/// </summary>
+public sealed class NoSmallMobsCollisionSystem : EntitySystem
+{
+    private EntityQuery<PhysicsComponent> _physicsQuery;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<MobCollisionComponent, MapInitEvent>(OnMapInit);
+
+        _physicsQuery = GetEntityQuery<PhysicsComponent>();
+    }
+
+
+    private void OnMapInit(Entity<MobCollisionComponent> ent, ref MapInitEvent args)
+    {
+        if (!_physicsQuery.TryComp(ent, out var physics))
+            return;
+
+        // мелкие мобы не должны сталкиваться, в идеале сделать столкновение мелкие-мелкие и большие-большие,
+        // но ради оптимизации можно просто вырубить ее у мелких, все равно всем похуй будет
+        if ((physics.CollisionMask & (int)CollisionGroup.SmallMobMask) != 0)
+            RemComp<MobCollisionComponent>(ent);
+    }
+}
