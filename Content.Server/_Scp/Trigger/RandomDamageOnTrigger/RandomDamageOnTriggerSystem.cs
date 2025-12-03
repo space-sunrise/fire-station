30e9024f91956273793430ca0dc8f3bd17ff08d5
ThereDrD <drdtheuser@gmail.com>
2025-12-03 13:40:21 +0300

    add: random damage system

---
+++ Content.Server/_Scp/Trigger/RandomDamageOnTrigger/RandomDamageOnTriggerSystem.cs
@@ -0,0 +1,46 @@
+﻿using Content.Server.Destructible;
+using Content.Shared._Scp.Trigger.RandomDamageOnTrigger;
+using Content.Shared.Damage;
+using Content.Shared.Trigger;
+using Robust.Shared.Random;
+
+namespace Content.Server._Scp.Trigger.RandomDamageOnTrigger;
+
+public sealed class RandomDamageOnTriggerSystem : EntitySystem
+{
+    [Dependency] private readonly DamageableSystem _damageable = default!;
+    [Dependency] private readonly DestructibleSystem _destructible = default!;
+    [Dependency] private readonly IRobustRandom _random = default!;
+
+    private readonly DamageSpecifier _tempDamage = new ();
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<RandomDamageOnTriggerComponent, TriggerEvent>(OnTrigger);
+
+        Log.Level = LogLevel.Info;
+    }
+
+    private void OnTrigger(Entity<RandomDamageOnTriggerComponent> ent, ref TriggerEvent args)
+    {
+        if (args.Key != null && !ent.Comp.KeysIn.Contains(args.Key))
+            return;
+
+        if (!_destructible.TryGetDestroyedAt(ent.Owner, out var destroyed))
+        {
+            Log.Warning($"Tried to trigger {nameof(RandomDamageOnTriggerComponent)} for entity with {nameof(DestructibleComponent)}! " +
+                        $"Entity is {ToPrettyString(ent)}, Prototype: {Prototype(ent)}");
+            return;
+        }
+
+        foreach (var type in ent.Comp.DamageTypes)
+        {
+            _tempDamage.DamageDict[type] =  destroyed.Value * _random.NextFloat(0f, ent.Comp.MaxDamagePercent);
+        }
+
+        _damageable.TryChangeDamage(ent, _tempDamage, ent.Comp.IgnoreResistancesForDamage);
+        _tempDamage.DamageDict.Clear();
+    }
+}
---
+++ Content.Shared/_Scp/Trigger/RandomDamageOnTrigger/RandomDamageOnTriggerComponent.cs
@@ -0,0 +1,19 @@
+﻿using Content.Shared.Damage.Prototypes;
+using Content.Shared.Trigger.Components.Effects;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._Scp.Trigger.RandomDamageOnTrigger;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class RandomDamageOnTriggerComponent : BaseXOnTriggerComponent
+{
+    [DataField(required: true)]
+    public List<ProtoId<DamageTypePrototype>> DamageTypes;
+
+    [DataField]
+    public bool IgnoreResistancesForDamage;
+
+    [DataField]
+    public float MaxDamagePercent = 0.7f;
+}
