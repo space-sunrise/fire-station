f3d5a6df7ad0220183d77b9211546272ca146dd1
666XxttimurkaxX666 <666qwe3@gmail.com>
2026-01-05 12:34:38 +0700

    сцп 330
    система сцп 330

---
+++ Content.Server/_Scp/Scp330/Scp330Component.cs
@@ -0,0 +1,21 @@
+﻿namespace Content.Server.Scp330;
+
+[RegisterComponent]
+public sealed partial class Scp330Component : Component
+{
+    [DataField("regenDelay")]
+    public float RegenDelay = 10f;
+
+    public float Accumulator = 0f;
+
+    [DataField("maxCandies")]
+    public int MaxCandies = 10;
+    
+    // количество конфет в миске
+    public int CurrentCandies = 10;
+
+    [DataField("baseDamage")]
+    public float BaseDamage = 20f;
+
+    public Dictionary<EntityUid, int> ThiefCounter = new();
+}
