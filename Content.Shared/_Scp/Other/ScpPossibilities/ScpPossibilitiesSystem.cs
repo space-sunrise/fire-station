d3acacb40a510506f73c7c7528dfc228e1f4e658
Wardex <grichanijbogdan@gmail.com>
2026-04-25 20:03:52 +0300

    Доработка

--- Content.Shared/_Scp/Other/ScpPossibilities/ScpPossibilitiesSystem.cs
+++ Content.Shared/_Scp/Other/ScpPossibilities/ScpPossibilitiesSystem.cs
@@ -2,33 +2,38 @@
 using Content.Shared.Mech;
 using Content.Shared.Mech.Components;
 using Content.Shared.Interaction.Events;
+using Content.Shared.Weapons.Melee.Events;
+using System.Linq;
 
 namespace Content.Shared._Scp.Other.ScpPossibilities;
 
 public sealed partial class ScpPossibilitiesSystem : EntitySystem
 {
-    private EntityQuery<MechComponent> _mechComp;
     public override void Initialize()
     {
         base.Initialize();
 
-        _mechComp = GetEntityQuery<MechComponent>();
-
-        SubscribeLocalEvent<ScpPossibilitiesComponent, AttackAttemptEvent>(OnAttackAttempt);
+        SubscribeLocalEvent<ScpPossibilitiesComponent, MeleeHitEvent>(OnAttackAttempt);
     }
 
-    public void OnAttackAttempt(Entity<ScpPossibilitiesComponent> ent, ref AttackAttemptEvent args)
+    public void OnAttackAttempt(Entity<ScpPossibilitiesComponent> ent, ref MeleeHitEvent args)
     {
-        if (args.Target == null)
-            return;
-
-        if (!_mechComp.TryComp(args.Target.Value, out var _))
+        if (!args.HitEntities.Any())
             return;
 
         if (!ent.Comp.CanEjectPilotFromMech)
             return;
 
-        var ev = new MechEjectPilotEvent();
-        RaiseLocalEvent(args.Target.Value, ev);
+        foreach (var target in args.HitEntities)
+        {
+            if (!TryComp<MechComponent>(target, out var mechComp))
+                continue;
+
+            if (mechComp.PilotSlot.ContainedEntities == null)
+                continue;
+
+            var ev = new MechEjectPilotEvent();
+            RaiseLocalEvent(target, ev);
+        }
     }
 }
