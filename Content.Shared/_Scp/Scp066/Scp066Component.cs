579906c6f9658f2dd50f50e6c9f111d442352a21
666XxttimurkaxX666 <666qwe3@gmail.com>
2026-04-28 19:22:36 +0700

    пекыпекрекркр

--- Content.Shared/_Scp/Scp066/Scp066Component.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Content.Shared._Scp.Scp066;
-
-[RegisterComponent]
-public sealed partial class Scp066FriendlyComponent : Component { }
-
-[RegisterComponent]
-public sealed partial class Scp066HostileComponent : Component { }
