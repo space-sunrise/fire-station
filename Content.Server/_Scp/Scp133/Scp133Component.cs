65dea1bc031784e0e2649bea6caab1472b265b66
666XxttimurkaxX666 <666qwe3@gmail.com>
2026-05-27 15:16:36 +0700

    Add scp 133

---
+++ Content.Server/_Scp/Scp133/Scp133Component.cs
@@ -0,0 +1,19 @@
+﻿using Content.Shared.Damage;
+
+namespace Content.Server._Scp.Scp133;
+
+[RegisterComponent]
+public sealed partial class Scp133Component : Component
+{
+    [DataField]
+    public DamageSpecifier Damage = new()
+    {
+        DamageDict = new() { { "Structural", 200 } }
+    };
+
+    [DataField]
+    public float Delay = 2.5f;
+
+    [DataField]
+    public bool DeleteAfter = true;
+}
---
+++ Content.Server/_Scp/Scp133/Scp133System.cs
@@ -0,0 +1,39 @@
+﻿using Content.Shared.Damage;
+using Content.Shared.Damage.Systems;
+using Content.Shared.Sticky.Components;
+using Content.Shared.Trigger;
+using Robust.Shared.Timing;
+
+namespace Content.Server._Scp.Scp133;
+
+public sealed class Scp133System : EntitySystem
+{
+    [Dependency] private readonly DamageableSystem _damageable = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<Scp133Component, TriggerEvent>(OnTrigger);
+    }
+
+    private void OnTrigger(EntityUid uid, Scp133Component component, TriggerEvent args)
+    {
+        if (!TryComp<StickyComponent>(uid, out var sticky) || sticky.StuckTo == null)
+            return;
+
+        var target = sticky.StuckTo.Value;
+        var delayTime = TimeSpan.FromSeconds(component.Delay);
+
+        Timer.Spawn(delayTime, () =>
+        {
+            if (!Exists(target) || !Exists(uid))
+                return;
+
+            _damageable.TryChangeDamage(target, component.Damage, ignoreResistances: true);
+
+            if (component.DeleteAfter)
+                QueueDel(uid);
+        });
+    }
+}
