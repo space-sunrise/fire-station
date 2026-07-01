0efa3025ce30065c534c0adadd40b28ac998ad38
Wardex <grichanijbogdan@gmail.com>
2026-07-01 17:52:40 +0300

    1

--- Content.Server/_Scp/GameTicking/Rules/Components/ChaosSleepSpyRuleComponent.cs
+++ Content.Server/_Scp/GameTicking/Rules/Components/ChaosSleepSpyRuleComponent.cs
@@ -5,9 +5,6 @@ namespace Content.Server._Scp.GameTicking.Rules.Components;
 [RegisterComponent, Access(typeof(ChaosSpyRuleSystem), typeof(ChaosSleepSpyRuleSystem))]
 public sealed partial class ChaosSleepSpyRuleComponent : Component
 {
-    [DataField(required: true)]
-    public AntagSelectionDefinition Definition;
-
     [DataField]
     public Color CodeWordColor = Color.FromHex("#cc3b3b");
 
