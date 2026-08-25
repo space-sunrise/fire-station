cd44f779735e911f19d2b2f005b5a03f38de48af
bruhmogus <104110869+bruhmogus@users.noreply.github.com>
2026-08-24 19:33:26 -0500

    this wont work but i might as well try
    attempt to fix build error by removing whats causing the build error

--- Content.Server/Entry/EntryPoint.cs
+++ Content.Server/Entry/EntryPoint.cs
@@ -114,8 +114,7 @@ namespace Content.Server.Entry
                 var cast = (ServerModuleTestingCallbacks)callback;
                 cast.ServerBeforeIoC?.Invoke();
             }
-
-            Dependencies.Resolve<IRobustSerializer>().FloatFlags = SerializerFloatFlags.RemoveReadNan;
+            
         }
 
         /// <inheritdoc />
