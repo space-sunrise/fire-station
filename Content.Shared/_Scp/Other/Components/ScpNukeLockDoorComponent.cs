ed028bc7cf6b74526166608d15c9fd5d09f80347
Wardex <grichanijbogdan@gmail.com>
2026-09-04 17:41:00 +0300

    Исправление ошибок карты

---
+++ Content.Shared/_Scp/Other/Components/ScpNukeLockDoorComponent.cs
@@ -0,0 +1,6 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._Scp.Other.Components;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class ScpNukeLockDoorComponent : Component;
