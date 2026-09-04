0e6b1280ba955e3893f071d95707b304b6cb1a91
Wardex <grichanijbogdan@gmail.com>
2026-09-04 17:43:05 +0300

    Случайно пушнул свои файлы

--- Content.Shared/_Scp/Other/Components/ScpNukeLockDoorComponent.cs
+++
@@ -1,6 +0,0 @@
-using Robust.Shared.GameStates;
-
-namespace Content.Shared._Scp.Other.Components;
-
-[RegisterComponent, NetworkedComponent]
-public sealed partial class ScpNukeLockDoorComponent : Component;
